7bec762b4ac73b8170e382b0083f68d4023cb0e6
tumainilyimo <lyimotumaini@gmail.com>
2021-06-18 14:52:22 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -11,7 +11,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/16x6yvjjyqwe1tz/export.pkl?dl=1'
+export_file_url = 'https://www.dropbox.com/s/ghzrst1f8d5lp2g/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['cocci', 'salmo', 'healthy']
